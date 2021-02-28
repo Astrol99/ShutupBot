7b82682703d0390dc8cec601fc423c763c2e7d69
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-02-27 18:32:48 -0600

    Added join and leave commands

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 require('dotenv').config();
+const PREFIX = process.env.PREFIX;
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.login(process.env.TOKEN);
+
 client.once('ready', () => {
-    console.log('Discord Bot Ready')
+    console.log(`Discord Bot Ready\nLogged in as ${client.user.username}`)
 });
 
-client.login(process.env.TOKEN);
+client.on('message', async message => {
+    if (message.content === `${PREFIX}ping`) {        
+        
+        message.channel.send(`Pong: ${client.ws.ping} ms`);
+
+    } else if (message.content === `${PREFIX}join`) {
+        
+        if (message.member.voice.channel){
+            const connection = await message.member.voice.channel.join();
+            message.channel.send(`Joined channel \`${message.member.voice.channel}\``)
+        } else {
+            message.channel.send(`User is not in channel!`)
+        }
+
+    } else if (message.content === `${PREFIX}leave`){
+
+        if (message.member.voice.channel){
+            await message.member.voice.channel.leave();
+            message.channel.send(`Left channel \`${message.member.voice.channel}\``)
+        } else {
+            message.channel.send(`User is not in channel!`)
+        }
+
+    }
+});
